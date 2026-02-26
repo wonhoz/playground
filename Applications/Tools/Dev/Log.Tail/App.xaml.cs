025966f06ea02732047998fc04ca64fae2002ec7
wonhokang <wonho.kang@smartcareworks.com>
2026-02-26 11:58:43 +0900

    [log.tail] | 다크 테마 + 전역 스타일
    GlobalUsings.cs, App.xaml (터미널 그린 #22C55E 다크 테마 — ScrollBar/Button/TextBox/ComboBox/CheckBox/TabControl/ListBox ControlTemplate)

---
+++ Applications/Tools/Dev/Log.Tail/App.xaml.cs
@@ -0,0 +1,5 @@
+namespace LogTail;
+
+public partial class App : Application
+{
+}
---
+++ Applications/Tools/Dev/Log.Tail/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using System.Collections.ObjectModel;
+global using System.IO;
+global using System.Runtime.InteropServices;
+global using System.Text.RegularExpressions;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Input;
+global using System.Windows.Interop;
+global using System.Windows.Media;
+global using LogTail.Models;
+global using LogTail.Services;
